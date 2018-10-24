4596e454b0f35a94ff8d89f69755678cc953a41f
Stefan Đekanović <stefan.dekanovic2@gmail.com>
2018-10-24 22:23:14 +0200

    Finished 1st w/ graph error on y-axis.

--- prva.R
+++ prva.R
@@ -1,6 +1,7 @@
 library("readr")
 library("dplyr")
 library("tidyr")
+library("ggplot2")
 
 # Obdobje: 2015-2017
 # Tip terminske obrestne mere: 3x6
@@ -11,42 +12,66 @@ eu15 <- read_csv("EURIBOR_2015.csv")
 eu16 <- read_csv("EURIBOR_2016.csv")
 eu17 <- read_csv("EURIBOR_2017.csv")
 
-second <- eu15
+# Čiščenje in urejanje podatkov
 
-# Čiščenje podatkov
-
-eu15 <- eu15[, c("X1", "02/01/2015", "02/02/2015", "02/03/2015", "01/04/2015", "04/05/2015", "01/06/2015",
-             "01/07/2015", "03/08/2015", "01/09/2015", "01/10/2015", "02/11/2015", "01/12/2015")]
-eu16 <- eu16[, c("X1", "04/01/2016", "01/02/2016", "01/03/2016", "01/04/2016", "02/05/2016", "01/06/2016",
-                 "01/07/2016", "01/08/2016", "01/09/2016", "03/10/2016", "01/11/2016", "01/12/2016")]
-eu17 <- eu17[, c("X1", "02/01/2017", "01/02/2017", "01/03/2017", "03/04/2017", "02/05/2017", "01/06/2017",
-                 "03/07/2017", "01/08/2017", "01/09/2017", "02/10/2017", "01/11/2017", "01/12/2017")]
-
-eu15 <- t(eu15)
-colnames(eu15) <- c("1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m")
-eu15 <- eu15[-1,]
-
-eu16 <- t(eu16)
-colnames(eu16) <- c("1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m")
-eu16 <- eu16[-1,]
-
-eu17 <- t(eu17)
-colnames(eu17) <- c("1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m")
-eu17 <- eu17[-1,]
+#eu15
 
 mesec <- 1
 k <- 2
 v <- 1
 while (mesec < 13) {
-  if (as.numeric(unlist(strsplit(colnames(eu15)[k], split = "/"))[2]) != mesec) {v <- c(v)};
   if (as.numeric(unlist(strsplit(colnames(eu15)[k], split = "/"))[2]) == mesec) {mesec <- mesec+1; v <- c(v, k)};
   k <- k+1
 }
 
+eu15 <- eu15[v]
+eu15 <- t(eu15)
+colnames(eu15) <- c("1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m")
+eu15 <- eu15[-1,]
+eu15 <- data.frame(eu15)
 
-#mesec = 1
-#k = 1
-#if (as.numeric(strsplit(colnames(eu15)[k]), split = "/") != mesec) {eu15 <- eu15[,-k]};
-#if (as.numeric(strsplit(colnames(eu15)[k]), split = "/") == mesec) {mesec <- mesec + 1};
-#k <- k+1;
-#if (length(colnames(second)) <= 13) break
+#eu16
+
+mesec <- 1
+k <- 2
+v <- 1
+while (mesec < 13) {
+  if (as.numeric(unlist(strsplit(colnames(eu16)[k], split = "/"))[2]) == mesec) {mesec <- mesec+1; v <- c(v, k)};
+  k <- k+1
+}
+
+eu16 <- eu16[v]
+eu16 <- t(eu16)
+colnames(eu16) <- c("1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m")
+eu16 <- eu16[-1,]
+eu16 <- data.frame(eu16)
+
+#eu17
+
+mesec <- 1
+k <- 2
+v <- 1
+while (mesec < 13) {
+  if (as.numeric(unlist(strsplit(colnames(eu17)[k], split = "/"))[2]) == mesec) {mesec <- mesec+1; v <- c(v, k)};
+  k <- k+1
+}
+
+eu17 <- eu17[v]
+eu17 <- t(eu17)
+colnames(eu17) <- c("1w", "2w", "1m", "2m", "3m", "6m", "9m", "12m")
+eu17 <- eu17[-1,]
+eu17 <- data.frame(eu17)
+
+# Graf: sprememba 3x6 obrestne mere glede na čas
+
+X <- eu15[,5:6]
+Y <- eu16[,5:6]
+Z <- eu17[,5:6]
+XYZ <- rbind(X, Y, Z)
+
+graf <- ts.plot(ts(data=XYZ, start=c(2015,1,2), end=c(2017,12,1), frequency = 12), gpars=list(xlab="Time", ylab="%"), col=c("red", "blue"));
+        title(main="Euribor"); 
+        legend("topleft", legend = c("3m", "6m"), col=c("red", "blue"), lty = c(1,1))
+
+
+
