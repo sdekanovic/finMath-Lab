41cfcf951058f05feea0c091fe596072b4c3c0b6
Stefan Đekanović <stefan.dekanovic2@gmail.com>
2018-11-30 13:41:46 +0100

    cetrta.R

--- Glajenje časovnih vrst/cetrta.R
+++ Glajenje časovnih vrst/cetrta.R
@@ -21,25 +21,87 @@ points(ts(cene), p = 20)
 
 G <- function(vrsta, k){
   
-  y <- numeric(length(vrsta) - k)
-  # for i in k+1: length(y) + 1
-  for (i in 1:length(y)){
-    # k + (k-1) + ... + 1
-    # k+1 + k + (k-1) + ... 2
-    vrednost <- 0
-    vrednost <- sum(vrsta[c(i:(k+i-1))])/k
-    y[i] <- vrednost
+  y <- numeric(length(vrsta))
+  for (i in 1:k){
+    y[i] <- NA
+  }
+  for (i in k:length(vrsta)){
+    # t = k:k + (k-1) + ... + 1
+    # t = k+1: k+1 + k + (k-1) + ... 2
+    y[i+1] <- sum(vrsta[c((i-k+1):(i))])/k
   }
   return(y)
 }
 
 zglajena <- G(cene, 5)
-napoved <- sum(cene[c(119:123)])/5
+napoved <- zglajena[length(zglajena)]
 
-par(new = TRUE)
-plot(ts(zglajena), from = 5, to = 123, col = "red")
+vrsta1 <- ts(cene)
+vrsta2 <- ts(zglajena)
+ts.plot(vrsta1, vrsta2, col = c("black", "red"), main = "Drsno povprečje reda 5", ylab = "$")
+points(vrsta1, p = 20)
 
-a<-ts(zglajena)
-b<-ts(cene)
-ts.plot(b,a)
+MSE <- function(vrsta, k = 1){
+  zglajena <- G(vrsta, k)
+  vsota <- 0
+  for (i in k:(length(vrsta) - 1)){
+    vsota <- vsota + (vrsta[i+1]-zglajena[i+1])**2
+  }
+  vsota <- vsota / (length(vrsta) - k)
+  return(vsota)
+}
 
+MSE(cene, 5)
+
+zglajena15 <- G(cene, 15)
+napoved15 <- zglajena15[length(zglajena15)]
+vrsta15 <- ts(zglajena15)
+ts.plot(vrsta1, vrsta15, col = c("black", "red"), main = "Drsno povprečje reda 15", ylab = "$")
+points(vrsta1, p = 20)
+MSE(cene, 15)
+
+zglajena30 <- G(cene, 30)
+napoved30 <- zglajena30[length(zglajena30)]
+vrsta30 <- ts(zglajena30)
+ts.plot(vrsta1, vrsta30, col = c("black", "red"), main = "Drsno povprečje reda 30", ylab = "$")
+points(vrsta1, p = 20)
+MSE(cene, 30)
+
+# narisi skupaj grafe
+ 
+# Naloga 3: Eksponentno glajenje
+
+EG <- function(vrsta, alpha){
+  # y^_T+1 = l_t
+  l <- numeric(length(vrsta))
+  l[1] <- vrsta[1]
+  for (i in 2:(length(vrsta))){
+    l[i] <- alpha*vrsta[i] + (1 - alpha)*l[i-1]
+  }
+  return(l)
+}
+
+alfa <- 0.1
+zglajenaE <- EG(cene, alfa)
+napovedE <- zglajenaE[length(zglajenaE)]
+napovedE
+
+vrsta1 <- ts(cene)
+vrstaE <- ts(zglajenaE)
+ts.plot(vrsta1, vrstaE, col = c("black", "red"), main = "Eksponentno glajenje", ylab = "$")
+points(ts(cene), p = 20)
+
+MSEa <- function(alfa){
+  vrsta <- EG(cene, alfa)
+  return(MSE(vrsta, k = 1))
+}
+
+optimize(MSEa, c(0, 1))
+
+alfaOpt <- optimize(MSEa, c(0, 1))
+alfaOpt <- as.double(alpha)[1]
+zglajenaE <- EG(cene, alfaOpt)
+vrsta1 <- ts(cene)
+vrstaE <- ts(zglajenaE)
+ts.plot(vrsta1, vrstaE, col = c("black", "red"), main = "Eksponentno glajenje, minimalen MSE", ylab = "$")
+points(ts(cene), p = 20)
